700b7a360236569533cd1ce88ccdf79109130017
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-10-26 18:45:12 +0900

    feat: カスタマイズビューでUI Component利用

--- src/apps/app/main.ts
+++ src/apps/app/main.ts
@@ -1,54 +1,76 @@
 import { Dropdown, Table } from 'kintone-ui-component@next'
-import type { DetailEvent } from 'types'
+import type { IndexEvent } from 'types'
 
-kintone.events.on(['app.record.detail.show'], async (event: DetailEvent<any /* kintone.types.SavedXxxxFields */>) => {
-  const body = kintone.app.record.getSpaceElement('space')!
-  const renderAge = (dataCell: any, dataRow: any) => {
-    const element = document.createElement('h3')
-    element.innerText = `The age is ${dataCell}`
-    return element
-  }
-  const renderName = (cellData: any) => {
-    const dropdown = new Dropdown({
-      //  items: [...],
-      value: cellData,
-      selectedIndex: 0,
-    })
-    return dropdown
-  }
+const renderLink = (recordId: number) => {
+  const element = document.createElement('p')
+  element.innerHTML = `<a href="/k/4771/show#record=${recordId}">📄</a>`
+  return element
+}
+const renderItem = (cellData: string) => {
+  const dropdown = new Dropdown({
+    items: [
+      { label: 'コーラ', value: 'コーラ' },
+      { label: 'メロンソーダ', value: 'メロンソーダ' },
+      { label: 'コーヒー', value: 'コーヒー' },
+    ],
+    value: cellData,
+    selectedIndex: 0,
+  })
+  return dropdown
+}
+
+kintone.events.on('app.record.index.show', async (event: IndexEvent<kintone.types.SavedAppFields>) => {
+  const records = event.records
 
   const table = new Table({
+    id: 'customize-view-table',
+    label: 'kintone UI Componentで作ったカスタマイズビューでございまーす！',
+    actionButton: false,
     columns: [
       {
-        title: 'Name',
-        field: 'name',
-        render: renderName,
+        title: '',
+        field: 'link',
+        render: renderLink,
       },
       {
-        title: 'Address',
-        field: 'address',
+        title: '会社名',
+        field: '会社名',
       },
       {
-        title: 'Age',
-        field: 'age',
-        render: renderAge,
+        title: '商品名',
+        field: '商品名',
+        render: renderItem,
+      },
+      {
+        title: '単価',
+        field: '単価',
+      },
+      {
+        title: '数量',
+        field: '数量',
+      },
+      {
+        title: '金額',
+        field: '金額',
       },
     ],
-    data: [
-      {
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-      },
-      {
-        name: 'voduchau',
-        age: 20,
-        address: 'New York No. 1 Lake Park',
-      },
-    ],
-    label: 'Editable Table',
+    data: records.map((record) => ({
+      link: record.$id.value,
+      会社名: record.会社名.value,
+      商品名: record.商品名.value,
+      単価: record.単価.value,
+      数量: record.数量.value,
+      金額: record.金額.value,
+    })),
   })
-  body.appendChild(table)
+
+  const space = document.querySelector<HTMLDivElement>('#customize-view')!
+  space.style.padding = '1rem'
+  const currentTable = document.querySelector('#customize-view-table')
+  if (currentTable) {
+    space.removeChild(currentTable)
+  }
+  space.appendChild(table)
 
   return event
 })
